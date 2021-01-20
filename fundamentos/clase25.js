76e14ae0f7c6cd8b459fa9bdc44e32474bad1b91
Angel <zangelweb@gmail.com>
2021-01-19 18:03:51 -0600

    clase de Clases chidas

--- fundamentos/clase25.js
+++ fundamentos/clase25.js
@@ -1,26 +1,30 @@
 class Persona{
+
     constructor(nombre, apellido, altura) {
         this.nombre = nombre
         this.apellido = apellido
         this.altura = altura
-    };
+    }
+
+    saludar(){
+        console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+    }
+
+    soyAlto(){
+        return this.altura > 1.75
+    }
 }
 
-Persona.prototype.saludar = function(){
-    console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+class Desarrollador extends Persona{
+
+    constructor(nombre, apellido, altura) {
+        super(nombre, apellido, altura)
+    }
+    
+    saludar(){
+        console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrolador`)
+    }
 }
 
-Persona.prototype.soyAlto = function(){
-    return this.altura > 1.75
-}
 
-function Desarrollador(nombre, apellido){
-    this.nombre = nombre
-    this.apellido = apellido
-}
-
-heredaDe(Desarrollador, Persona)
-
-Desarrollador.prototype.saludar = function(){
-    console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrolador`)
-}
+let angel = new Persona('Angel', 'Zaragoza', 1.79);
